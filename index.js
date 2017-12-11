7ad367b6561decaa187f0ec6ebc966e9e65e0d74
jjwumaster <jjwumaster@gmail.com>
2017-12-11 17:03:42 -0500

    turning it back over to connie

--- index.js
+++ index.js
@@ -20,27 +20,76 @@ fi = (function() {
       }
       return acc;
     },
-    find: function() {
+    find: function(collection, predicate) {
+      for (i in Object.keys(collection)) {
+        if (predicate(collection[i])) {
+          return collection[i];
+        }
+      }
+    },
+    filter: function(collection, predicate) {
+      let output = [];
+      for (i in Object.keys(collection)) {
+        if (predicate(collection[i])) {
+          output.push(collection[i]);
+        }
+      }
+      return output;
+    },
+    size: function(collection) {
+      let total = 0;
+      for (i in Object.keys(collection)){
+        total++;
+      }
+      return total;
+    },
+    first: function(array, n) {
+      let x = (n === undefined ? 0 : n);
+      return array.slice(0, x);
+    },
+    last: function(array, n) {
+      let x = (n === undefined ? 1 : n);
+      return array.slice(-x);
+    },
+    compact: function(array) {
+      let output = []
+      for (let i = 0; i < array.length; i++) {
+        if (!!array[i] !== false){
+          output.push(array[i]);
+        }
+      }
+      return output;
+    },
+    // refactor
+    sortBy: function(array, iteratee) {
+      let output = {}
+      let outputArray = []
+      let finalOutput = []
+      for (let i = 0; i < array.length; i++) {
+        output[iteratee(array[i])] = array[i];
+        outputArray.push(iteratee(array[i]))
+      }
+      for (i of outputArray.sort(function(a,b){
+        return a - b;
+      })){
+        finalOutput.push(output[i]);
+      }
+      return finalOutput;
+    },
+    flatten: function(){
 
     },
-    filter: function() {
+    uniq: function(){
 
     },
-    size: function() {
+    keys: function(){
 
     },
-    first: function() {
+    values: function(){
 
     },
-    last: function() {
+    functions: function(){
 
     }
-    compact: function() {
-
-    },
-    sortBy: function() {
-
-    },
-
   }
 })()
